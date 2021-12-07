da9201ac2cded253592fbd82bba0140c792654b4
Subhra Roy <subhra.roy@e-arc.com>
2021-12-07 16:57:20 +0530

    move all files under one roof

--- Package.swift
+++ Package.swift
@@ -6,22 +6,13 @@ import PackageDescription
 let package = Package(
     name: "ColorPicker",
     platforms: [
-        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ColorPicker",
-            targets: ["ColorPicker",
-                      "ARCHueColorPickerDataModel",
-                      "CheckMarkControls",
-                      "Extension",
-                      "Managers",
-                      "Model",
-                      "Paths",
-                      "Protocol",
-                      "Util",
-                      "View"]),
+            targets: ["ColorPicker"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -32,45 +23,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ColorPicker",
-            dependencies: ["ARCHueColorPickerDataModel",
-                           "CheckMarkControls",
-                           "Extension",
-                           "Managers",
-                           "Model",
-                           "Paths",
-                           "Protocol",
-                           "Util",
-                           "View",
-            ]),
-        .target(name: "ARCHueColorPickerDataModel",
-                dependencies: []),
-        .target(name: "CheckMarkControls",
-                dependencies: []),
-        .target(name: "Extension",
-                dependencies: []),
-        .target(name: "Managers",
-                dependencies: []),
-        .target(name: "Model",
-                dependencies: []),
-        .target(name: "Paths",
-                dependencies: []),
-        .target(name: "Protocol",
-                dependencies: []),
-        .target(name: "Util",
-                dependencies: []),
-        .target(name: "View",
-                dependencies: []),
+            dependencies: []),
+       
         .testTarget(
             name: "ColorPickerTests",
-            dependencies: ["ColorPicker",
-                           "ARCHueColorPickerDataModel",
-                           "CheckMarkControls",
-                           "Extension",
-                           "Managers",
-                           "Model",
-                           "Paths",
-                           "Protocol",
-                           "Util",
-                           "View"]),
+            dependencies: ["ColorPicker"]),
     ]
 )
